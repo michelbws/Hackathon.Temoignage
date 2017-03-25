f5227a3519dec19506d05441ab0754df0333b2c6
Nedim Sacic <nedim.sacic@gmail.com>
2017-03-25 18:49:51 -0400

    ajout DocumentDBAzure

---
+++ Temoignage.API/DocumentDBAzure/DocDbAzure.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Azure.Documents.Client;
+
+namespace Temoignage.API.DocumentDBAzure
+{
+    public static class DocDbAzure
+    {
+        static Uri docAccountUri = new Uri("https://temoignagedb.documents.azure.com:443/");
+        static DocumentClient docClient = new DocumentClient(docAccountUri, "2WxpSk5O18zS0dkvcBZrG6OFncEjWffMmnQhr84J2rfvZADVnNHEI1fX1XOGbnp4oCQEHG1zHHuULFMe9Ui1ZQ==");
+        static string dbId = "Temoingnage";
+        static string collId = "TemoingnageCollection";
+        static Uri dbUri = new Uri($"dbs/{dbId}", UriKind.Relative);
+        static Uri collUri = new Uri($"dbs/{dbId}/colls/{collId}", UriKind.Relative);
+
+        public static void UpsertDocument(TemoingnageJsn temoignage)
+        {
+            var response = docClient.UpsertDocumentAsync(collUri, temoignage, disableAutomaticIdGeneration: true).Result;
+        }
+
+        public static void InsertDocument(TemoingnageJsn temoignage)
+        {
+            var reponse = docClient.CreateDocumentAsync(collUri, temoignage).Result;
+
+        }
+
+    }
+}
