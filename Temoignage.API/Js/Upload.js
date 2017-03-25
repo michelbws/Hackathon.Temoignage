696c2dd4ce1f5c48fbe4f098ac0c87d33354aef7
Lordofsnails <yanming_hu1996@yahoo.ca>
2017-03-25 15:07:26 -0400

    Avail Azure, changes : added controller for files upload

---
+++ Temoignage.API/Js/Upload.js
@@ -0,0 +1,21 @@
+﻿
+
+$(document).ready(function () {
+    $('#btnUploadFile').on('click', function () {
+        var data = new FormData()
+        var files = $("#fileUpload").get(0).files;
+        // Add the uploaded image content to the form data collection
+        if (files.length > 0) {
+            data.append("UploadedImage", files[0]);
+        }
+        // Make Ajax request with the contentType = false, and procesDate = false
+        var ajaxRequest = $.ajax({
+            type: "POST",
+            url: "http://localhost:16158/api/FileUploads",
+            contentType: false,
+            processData: false,
+            data: data
+        });
+        ajaxRequest.done(function (xhr, textStatus) { });
+    });
+});
