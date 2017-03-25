100e28d1ed6107f6ab3edeb7a9a256457cf98ed5
Nedim Sacic <nedim.sacic@gmail.com>
2017-03-25 19:17:27 -0400

    Ajout DTO

---
+++ Temoignage.API/ModelDocument/DocumentDbDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Temoignage.API.ModelDocument
+{
+    public class DocumentDbDTO
+    {
+        public string Id { get; set; }
+        public string Gps { get; set; }
+        public   List<InfoImage> ListInfoImage { get; set; }
+
+        public List<InfoVideo> ListInfoVideo { get; set; }
+        public Int32 ratioImportante { get; set; }
+    }
+}
