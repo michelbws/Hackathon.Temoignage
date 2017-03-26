a1fbef8db3466253b7b59a3c48228b9c6969fbf2
MICHEL PROVENCAL <MICHEL.PROVENCAL@RADIO-CANADA.CA>
2017-03-26 12:07:28 -0400

    fix

--- Temoignage.API/Controllers/MichelController.cs
+++ Temoignage.API/Controllers/MichelController.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 
 namespace Temoignage.API.Controllers
 {
-    public class MichelController : Controller
+    public class MichelController : ApiController
     {
         // GET: Michel
         public string Index()
