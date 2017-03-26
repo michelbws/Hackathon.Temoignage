1030202a87bc8e95ddd7a5474837e6e02b229109
Lordofsnails <yanming_hu1996@yahoo.ca>
2017-03-26 11:25:59 -0400

    alert in commen

--- Temoignage.API/Controllers/AlertController.cs
+++ Temoignage.API/Controllers/AlertController.cs
@@ -1,22 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Net;
+//using System.Net.Http;
+//using System.Web.Http;
 
-namespace Temoignage.API.Controllers
-{
-    public class AlertController : ApiController
-    {
-        public Alert Get()
-        {
+//namespace Temoignage.API.Controllers
+//{
+//    public class AlertController : ApiController
+//    {
+//        public Alert Get()
+//        {
 
-        }
+//        }
 
-        public class Alert
-        {
+//        public class Alert
+//        {
 
-        }
-    }
-}
+//        }
+//    }
+//}
